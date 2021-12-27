98812eb1eb600e5ca96f0635ee069ed4751f94a6
Nicolas Talle <dev@nicolab.net>
2021-12-27 14:07:27 +0100

    use relative entry

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import commonjs from '@rollup/plugin-commonjs';
-import path from 'path';
 
 export default defineConfig({
   plugins: [commonjs()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
+      entry: './src/index.js',
       name: 'waitwait'
     },
     rollupOptions: {
